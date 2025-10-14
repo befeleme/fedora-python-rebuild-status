301b486818d47d5115bd2b9646533f2ee82d1280
Karolina Surma <ksurma@redhat.com>
2025-10-14 19:12:34 +0200

    Load data for 3.15

--- scripts/bugzillas.py
+++ scripts/bugzillas.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import bugzilla
 
@@ -6,29 +7,68 @@ from loaders import load_data, load_monitor_report, KOJI
 
 BUGZILLA = 'bugzilla.redhat.com'
 BZ_PAGE_SIZE = 20
-TRACKER = 2322407  # PYTHON3.14
-RAWHIDE = 2339432  # F43FTBFS
+
+# Python version configurations
+VERSION_CONFIG = {
+    "3.14": {
+        "tracker": 2322407,  # PYTHON3.14
+        "rawhide": 2339432,  # F43FTBFS
+        "failed_file": "data/failed_py314.pkgs",
+        "waiting_file": "data/waiting_py314.pkgs",
+        "python_pkgs": "data/python314.pkgs",
+        "copr_file": "data/copr_py314.pkgs",
+        "output_file": "data/bzurls_py314.json"
+    },
+    "3.15": {
+        "tracker": None,  # PYTHON3.15 tracker ID - to be created
+        "rawhide": 2339432,  # F44FTBFS - update when F44 tracker is created
+        "failed_file": "data/failed_py315.pkgs",
+        "waiting_file": "data/waiting_py315.pkgs",
+        "python_pkgs": "data/python315.pkgs",
+        "copr_file": "data/copr_py315.pkgs",
+        "output_file": "data/bzurls_py315.json"
+    }
+}
+
 BZAPI = bugzilla.Bugzilla(BUGZILLA)
 
-FAILED = load_data("data/failed.pkgs")
-if KOJI:
-    WAITING = load_data("data/waiting.pkgs")
-    FAILED.update(WAITING)
-# we only want to do Copr magic before the mass rebuild
-else:
-    HISTORICALLY_SUCCESSFUL = load_data("data/python314.pkgs")
-    ALL_IN_COPR = load_monitor_report("data/copr.pkgs")
 
-    # Attempt to find bugzillas for packages that
-    # started to fail to build just recently
-    FAILED.update(pkg for pkg in HISTORICALLY_SUCCESSFUL if ALL_IN_COPR.get(pkg) == "failed")
+def load_failed_packages(version="3.14"):
+    """Load failed packages for a specific Python version."""
+    config = VERSION_CONFIG[version]
 
-SORTED_FAILS = sorted(FAILED)
+    FAILED = load_data(config["failed_file"])
+    if KOJI:
+        WAITING = load_data(config["waiting_file"])
+        FAILED.update(WAITING)
+    # we only want to do Copr magic before the mass rebuild
+    else:
+        HISTORICALLY_SUCCESSFUL = load_data(config["python_pkgs"])
+        ALL_IN_COPR = load_monitor_report(config["copr_file"])
+
+        # Attempt to find bugzillas for packages that
+        # started to fail to build just recently
+        FAILED.update(pkg for pkg in HISTORICALLY_SUCCESSFUL if ALL_IN_COPR.get(pkg) == "failed")
+
+    return sorted(FAILED)
 
 
-def bugzillas():
+def bugzillas(sorted_fails, tracker, rawhide):
+    """Query Bugzilla for open bugs blocking the tracker or rawhide."""
     query = BZAPI.build_query(product='Fedora')
-    query['blocks'] = [TRACKER, RAWHIDE]
+
+    # Only add trackers that are not None
+    blocks = []
+    if tracker:
+        blocks.append(tracker)
+    if rawhide:
+        blocks.append(rawhide)
+
+    if not blocks:
+        # If no trackers, return empty list
+        return []
+
+    query['blocks'] = blocks
     query['limit'] = BZ_PAGE_SIZE
     query['offset'] = 0
     results = []
@@ -37,18 +77,31 @@ def bugzillas():
         query['offset'] += BZ_PAGE_SIZE
     results += partial
     return [b for b in sorted(results, key=lambda b: -b.id)
-            if b.is_open and b.component in SORTED_FAILS]
+            if b.is_open and b.component in sorted_fails]
 
 
-def map_pkgs_and_bzurls(bugzillas):
-    pkgs_urls = {pkg: {"urls": [], "summaries": []} for pkg in SORTED_FAILS}
-    for bug in bugzillas:
+def map_pkgs_and_bzurls(bugzillas_list, sorted_fails):
+    """Map packages to their Bugzilla URLs and summaries."""
+    pkgs_urls = {pkg: {"urls": [], "summaries": []} for pkg in sorted_fails}
+    for bug in bugzillas_list:
         pkgs_urls[bug.component]["urls"].append(bug.weburl)
         pkgs_urls[bug.component]["summaries"].append(bug.summary)
     return pkgs_urls
 
 
 if __name__ == "__main__":
-    pkg_url_mapping = map_pkgs_and_bzurls(bugzillas())
-    with open('data/bzurls.json', 'w') as open_file:
+    parser = argparse.ArgumentParser(description='Fetch Bugzilla URLs for failed Python packages')
+    parser.add_argument('--version', default='3.14', choices=['3.14', '3.15'],
+                        help='Python version to process (default: 3.14)')
+    args = parser.parse_args()
+
+    config = VERSION_CONFIG[args.version]
+    sorted_fails = load_failed_packages(args.version)
+
+    bugzillas_list = bugzillas(sorted_fails, config["tracker"], config["rawhide"])
+    pkg_url_mapping = map_pkgs_and_bzurls(bugzillas_list, sorted_fails)
+
+    with open(config["output_file"], 'w') as open_file:
         json.dump(pkg_url_mapping, open_file, indent=2)
+
+    print(f"Bugzilla URLs for Python {args.version} written to {config['output_file']}")
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,38 +1,71 @@
 #! /bin/bash -eu
 mkdir -p data
 
+##############################################################################
+# Python 3.14 Data Collection (from Koji - post-mass-rebuild)
+##############################################################################
+
 # get what was built with new python in copr
 # repoquery --repo=python314 --source --whatrequires 'libpython3.14.so.1.0()(64bit)' --whatrequires 'python(abi) = 3.14' --whatrequires 'python3.14dist(*)' | pkgname | env LANG=en_US.utf-8 sort | uniq > data/python314.pkgs
 
-# # post-mass-rebuild query - use koji now as a source for data
+# post-mass-rebuild query - use koji now as a source for data
 repoquery --repo=koji --source --whatrequires 'libpython3.14.so.1.0()(64bit)' --whatrequires 'python(abi) = 3.14' --whatrequires 'python3.14dist(*)' | pkgname | env LANG=en_US.utf-8 sort | uniq > data/python314.pkgs
 
 # get what's built with old python in koji
 repoquery --repo=koji --source --whatrequires 'libpython3.13.so.1.0()(64bit)' --whatrequires 'python(abi) = 3.13' --whatrequires 'python3.13dist(*)' | pkgname | env LANG=en_US.utf-8 sort | uniq > data/python313.pkgs
 
 # get what remains to be built
-env LANG=en_US.utf-8 comm -23 data/python313.pkgs data/python314.pkgs | grep -E -v '^(python3\.13)$' > data/todo.pkgs
+env LANG=en_US.utf-8 comm -23 data/python313.pkgs data/python314.pkgs | grep -E -v '^(python3\.13)$' > data/todo_py314.pkgs
 
 # get everything present in copr
-# copr monitor @python/python3.14 --output-format text-row --fields name,state | env LANG=en_US.utf-8 sort > data/copr.pkgs
-# cut -f1 data/copr.pkgs | sort > data/copr_names.pkgs
-# env LANG=en_US.utf-8 comm -23 data/todo.pkgs data/copr_names.pkgs > data/waiting.pkgs
-# env LANG=en_US.utf-8 comm -12 data/todo.pkgs data/copr_names.pkgs > data/failed.pkgs
-# rm data/copr_names.pkgs
+# copr monitor @python/python3.14 --output-format text-row --fields name,state | env LANG=en_US.utf-8 sort > data/copr_py314.pkgs
+# cut -f1 data/copr_py314.pkgs | sort > data/copr_names_py314.pkgs
+# env LANG=en_US.utf-8 comm -23 data/todo_py314.pkgs data/copr_names_py314.pkgs > data/waiting_py314.pkgs
+# env LANG=en_US.utf-8 comm -12 data/todo_py314.pkgs data/copr_names_py314.pkgs > data/failed_py314.pkgs
+# rm data/copr_names_py314.pkgs
 
 # post-mass-rebuild query
 # get the current progress and find the actual failures + blocked packages
-curl https://raw.githubusercontent.com/hroncok/whatdoibuild/python3.14/progress.pkgs |env LANG=en_US.utf-8 sort > data/progress.pkgs
-env LANG=en_US.utf-8 comm -12 data/progress.pkgs data/todo.pkgs > data/failed.pkgs
-env LANG=en_US.utf-8 comm -13 data/progress.pkgs data/todo.pkgs > data/waiting.pkgs
+curl https://raw.githubusercontent.com/hroncok/whatdoibuild/python3.14/progress.pkgs |env LANG=en_US.utf-8 sort > data/progress_py314.pkgs
+env LANG=en_US.utf-8 comm -12 data/progress_py314.pkgs data/todo_py314.pkgs > data/failed_py314.pkgs
+env LANG=en_US.utf-8 comm -13 data/progress_py314.pkgs data/todo_py314.pkgs > data/waiting_py314.pkgs
 
-repoquery -q --repo python314 python3.14 --latest-limit 1 > data/pyver
+repoquery -q --repo python314 python3.14 --latest-limit 1 > data/pyver_py314
+
+##############################################################################
+# Python 3.15 Data Collection (from Copr)
+##############################################################################
+
+# get what was built with python 3.15 in copr
+repoquery --repo=python315 --source --whatrequires 'libpython3.15.so.1.0()(64bit)' --whatrequires 'python(abi) = 3.15' --whatrequires 'python3.15dist(*)' | pkgname | env LANG=en_US.utf-8 sort | uniq > data/python315.pkgs
+
+# get everything present in copr for python 3.15
+copr monitor @python/python3.15 --output-format text-row --fields name,state | env LANG=en_US.utf-8 sort > data/copr_py315.pkgs
+cut -f1 data/copr_py315.pkgs | sort > data/copr_names_py315.pkgs
+
+# get what remains to be built (comparing with python 3.14 base)
+env LANG=en_US.utf-8 comm -23 data/python314.pkgs data/python315.pkgs | grep -E -v '^(python3\.14)$' > data/todo_py315.pkgs
+
+# determine waiting and failed packages for python 3.15
+env LANG=en_US.utf-8 comm -23 data/todo_py315.pkgs data/copr_names_py315.pkgs > data/waiting_py315.pkgs
+env LANG=en_US.utf-8 comm -12 data/todo_py315.pkgs data/copr_names_py315.pkgs > data/failed_py315.pkgs
+rm data/copr_names_py315.pkgs
+
+# get python 3.15 version from copr
+repoquery -q --repo python315 python3.15 --latest-limit 1 > data/pyver_py315
+
+##############################################################################
+# Common Data Collection
+##############################################################################
 
 # get packages and their respective maintainers
 curl https://src.fedoraproject.org/extras/pagure_owner_alias.json -o data/pagure_owner_alias.json
 
-# get bz urls for failed packages
+# get bz urls for failed packages (python 3.14)
 python3 scripts/bugzillas.py
+
+# get bz urls for failed packages (python 3.15)
+python3 scripts/bugzillas.py --version 3.15
 
 # download the most downloaded packages from PyPI
 wget https://hugovk.github.io/top-pypi-packages/top-pypi-packages.min.json -O data/top-pypi-packages.json
